77fc8dd026d4c6532a6fc09b21f5c5c24d31e0f9
jidapa tassanakul <onjidapa@windowslive.com>
2018-11-14 02:07:02 +0700

    hi!

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
 const path = require('path');
 const express = require('express');
-
+const ejs = require('ejs');
 const todos = require('./routes/todos');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//route
-app.get('/',(req,res) => { 
-    res.send('Hello World!');
-});
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// parse application/json
+app.use(express.json());
 
 app.post('/',function (req,res){
     res.send('Got a POST request');
 });
 
 app.use(express.static('public'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use('/todos', todos);
+// default route
+app.get('/',(req,res) =>{
+    res.render('index',{name:"Jidapa"});
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
